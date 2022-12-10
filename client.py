37868fb43d8144601d5044747c2df677698ef374
Said-belaroussi <said.belaroussi@ulb.be>
2022-12-10 16:32:09 +0100

    client is now a class + added initialisation protocol + initial pool of ip addresses

--- client.py
+++ client.py
@@ -1,30 +1,90 @@
+import pickle
 import socket
 import ipaddress
 import sys
+import rsa
 
-TOR_host = "127.0.0.1"
-TOR_port = 4000
+class Client:
 
+    list_addresses = []
+    public_key = 0
+    private_key = 0
 
-def init_node_as_relay():
-    """
-    Handshake between node and TOR server to decide on the relay IP address
+    def __init__(self, my_address, my_port, server_host, server_port):
+        self.tor_host = server_host
+        self.tor_port = server_port
+        self.my_address = my_address
+        self.my_port = my_port
 
-    :return:
-    """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.bind(("127.0.0.1", 6420))
-        sock.connect((TOR_host, TOR_port))
-        message = "Bonjour".encode()
-        sock.send(message)
-        address = sock.recv(1024)
-        address = address.decode()
-        print(f"Received address : {address}")
+    def init_keys(self):
+        (self.public_key, self.private_key) = rsa.newkeys(1024)
+    def send_encrypted_packet(self):
+        """
+        Add the path composed of IP addresses of at least 3 nodes and encrypt them with
+        the corresponding public keys and sends it to the first node.
+        :return:
+        """
 
-        return address
+    def encrypt_public_packet(self, packet, keys_list ) : # Probablement la même fonction qu'en dessous
+        """
+        Encrypts packet with the public keys of the nodes contained in the list
+        :return:
+        """
+
+    def decrypt_public_packet(self, packet, keys_list ) :
+        """
+        Decrypts received packet with the public keys of the nodes contained in the list
+        :return:
+        """
+
+    def decrypt_private_packet(self, packet) : # Probablement la même fonction qu'en dessous
+        """
+        Decrypts received packet with the private key
+        :return:
+        """
+
+    def encrypt_private_packet(self) :
+        """
+        Encrypts packet with the private key
+        :return:
+        """
+
+    def forward_packet(self, packet, IP_address):
+        """
+        Send the packet to the corresponding IP address
+        :return:
+        """
+
+    def connect_to_network(self):
+        """
+        Request a subset of IP addresses to a node already in the network
+        :return:
+        """
+    def init_node_as_relay(self):
+        """
+        Handshake between node and TOR server to decide on the relay IP address
+
+        :return:
+        """
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.bind((self.my_address, self.my_port))
+            sock.connect((self.tor_host, self.tor_port))
+            key = pickle.dumps(self.public_key)
+            message = key
+            sock.send(message)
+            addresses = sock.recv(2048)
+            addresses = pickle.loads(addresses)
+            print(f"Received address : {addresses}")
+
+            self.list_addresses = addresses
 
 
 if __name__ == '__main__':
-    ip_address = init_node_as_relay()
+    node = Client("127.0.0.8", 4000, "127.0.0.1", 4000)
+    node.init_keys()
+    node.init_node_as_relay()
+
+
+
 
 
--- server.py
+++ server.py
@@ -1,8 +1,14 @@
 import socket
 import sys
+import pickle
+
+import rsa
 
 host = "127.0.0.1"
 port = 4000
+
+# Each tuple of address is compose of (IP address, port number, is_exit_node)
+init_addresses = [("127.0.0.2", 4000, False), ("127.0.0.3", 4000, False), ("127.0.0.4", 4000, True)]
 
 if __name__ == '__main__':
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
@@ -10,9 +16,16 @@ if __name__ == '__main__':
         socket.listen()
         connection, address = socket.accept()
         with connection:
-            message = connection.recv(1024)
-            message = message.decode()
+            message = connection.recv(2048)
+            message = pickle.loads(message)
             print(f"Message received : {message}\n"
                   f"From : ({address[0]}, {address[1]})")
+            print(message)
+            if type(message) is rsa.PublicKey:
+                msg_to_node = pickle.dumps(init_addresses)
+                connection.send(msg_to_node)
+                print(message)
+                node_address = (address[0], address[1], message, False)
+                init_addresses.append(node_address)
+                print(init_addresses)
 
-            connection.send("127.0.0.2".encode())
